005c2d7fe66b057637fa0773daac2d11a3297698
steven liao <steven.liao@quantatw.com>
2019-05-21 18:45:20 +0800

    Update install-jetson-inference.sh

--- install-jetson-inference.sh
+++ install-jetson-inference.sh
@@ -8,3 +8,9 @@ cmake ../
 cd jetson-inference/build			# omit if pwd is already /build from above
 make
 sudo make install
+
+# follow this https://github.com/dusty-nv/jetson-inference/blob/master/docs/building-repo.md
+# Doc https://github.com/dusty-nv/jetson-inference
+# Image classifier : https://github.com/dusty-nv/jetson-inference/blob/master/docs/imagenet-console.md
+# detect object : https://github.com/dusty-nv/jetson-inference/blob/master/docs/detectnet-console.md
+# Segment : https://github.com/dusty-nv/jetson-inference/blob/master/docs/segnet-console.md
